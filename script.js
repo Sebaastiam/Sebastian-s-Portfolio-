33f9eafc96be6ce8a4f0d6fd831835899453bd02
Sebaastiam <puebloazuliza@gmail.com>
2026-06-10 02:30:19 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 (function () {
   const slides  = Array.from(document.querySelectorAll('.bg-slide'));
   const bgImgs  = Array.from(document.querySelectorAll('.bg-img'));
-  const animSeq = ['panLeft','panUp','panUp','panDown','panRight','zoomOut','panRight','panDown'];
+  const animSeq = ['panLeft','panUp','panUp','none','panDown','zoomOut','panRight','none'];
   let current = 0, started = false;
   function applyAnim(i) {
     const img = bgImgs[i];
